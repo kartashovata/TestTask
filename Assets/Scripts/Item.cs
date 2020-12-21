87a17542d9eee498f03472552691d9a292493120
kartashovata <kartashovata@gmail.com>
2020-12-21 15:53:36 +0100

    init

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Item : Interactible
+{
+    [SerializeField] private string _name;
+    [SerializeField] private int _price;
+
+    public event UnityAction<Item> CalledFromShelf;
+    public event UnityAction<Item> Destroyed;
+
+    public string Name => _name;
+    public int Price => _price;
+
+    public override void Interact()
+    {
+        if (transform.parent.gameObject.TryGetComponent(out Counter counter))
+        {
+            //delete object from counter
+            Destroyed?.Invoke(this);
+            Destroy(gameObject);
+        }
+        else
+        {
+            //announce that object is called
+            CalledFromShelf?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class Person : Interactible
+{
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private Text _text;
+
+    private Animator _animator;
+    
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        Interact();
+    }
+
+    public override void Interact()
+    {
+        //wave
+        _animator.SetTrigger("Wave");
+        Say("Hello!\nClick at an item on the shelf to put it on the counter.\nClick at item on the counter to remove it. \nClick on the counter to get info about items on it.");
+    }
+
+    public void Say(string text)
+    {
+        //say stuff
+        _text.text = text;
+    }
+}
